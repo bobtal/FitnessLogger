5852fd19aba01a14a0afec7f7fae1bf65103d160
bobtal <talevski.boban@gmail.com>
2019-05-12 17:00:12 +0200

    Add TrainingSession POJO entity class

---
+++ app/src/main/java/com/gmail/at/boban/talevski/fitnesslogger/database/TrainingSessionEntry.java
@@ -0,0 +1,41 @@
+package com.gmail.at.boban.talevski.fitnesslogger.database;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+
+import java.util.Date;
+
+@Entity
+public class TrainingSessionEntry {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private Date date;
+
+    public TrainingSessionEntry(int id, Date date) {
+        this.id = id;
+        this.date = date;
+    }
+
+    @Ignore
+    public TrainingSessionEntry(Date date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
