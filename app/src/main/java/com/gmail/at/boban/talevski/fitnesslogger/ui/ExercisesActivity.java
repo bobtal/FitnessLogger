b512ec08c299d35114a6a2476fbff19c16d41d82
bobtal <talevski.boban@gmail.com>
2019-05-26 15:02:44 +0200

    Show exercises in ExerciseActivity
    Add recycler view initialization and hooking it with the adapter to show the exercises

--- app/src/main/java/com/gmail/at/boban/talevski/fitnesslogger/ui/ExercisesActivity.java
+++ app/src/main/java/com/gmail/at/boban/talevski/fitnesslogger/ui/ExercisesActivity.java
@@ -4,16 +4,37 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.gmail.at.boban.talevski.fitnesslogger.R;
+import com.gmail.at.boban.talevski.fitnesslogger.adapter.ExerciseAdapter;
+import com.gmail.at.boban.talevski.fitnesslogger.database.AppDatabase;
 
 public class ExercisesActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerViewExercises;
+    private ExerciseAdapter exerciseAdapter;
+
+    private AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercises);
+
+        // Initialize the database
+        db = AppDatabase.getInstance(getApplicationContext());
+
+        // Initialize the recycler view
+        recyclerViewExercises = findViewById(R.id.recyclerViewExercises);
+        // Set the recycler view layout
+        recyclerViewExercises.setLayoutManager(new LinearLayoutManager(this));
+
+        // Initialize the adapter and attach it to the recycler view
+        exerciseAdapter = new ExerciseAdapter(db.exerciseDao().loadAllExercises(), this);
+        recyclerViewExercises.setAdapter(exerciseAdapter);
 
         // Initialize the FAB and set its onClick listener
         FloatingActionButton fabAddExercise = findViewById(R.id.fabAddExercise);
